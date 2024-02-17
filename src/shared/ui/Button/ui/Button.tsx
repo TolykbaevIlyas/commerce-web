9f73501b6989ed52b2260ed5e537495fe20824f7
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-02-17 15:15:58 +0600

    Update Intro Block

--- src/shared/ui/Button/ui/Button.tsx
+++ src/shared/ui/Button/ui/Button.tsx
@@ -1,10 +1,11 @@
 'use client'
 import React from 'react'
 import { IButton } from '../types'
+import s from './assets/style/style.module.css'
 
 const Button = ({children,onClick,className,type}:IButton) => {
   return (
-    <button type={type} onClick={()=>onClick} className={`${className}`}>
+    <button type={type} onClick={()=>onClick} className={`${className} ${s.Button} px-10 py-5 font-bold`}>
         {children}
     </button>
   )
--- src/widgets/Intro/ui/Intro.tsx
+++ src/widgets/Intro/ui/Intro.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import s from '../assets/css/style.module.css'
+import { Button } from '@/shared/ui/Button'
 
 const Intro = () => {
   return (
     <main className={`${s.Intro}`}>
-        <div className={`${s.IntroBlock} `}>
-            a
+        <div className={`${s.IntroBlock} px-10 py-20`}>
+            <p className={`font-bold`}>New Arrival</p>
+            <h3 className={`font-bold text-5xl mt-5 ${s.IntroText}`}>Discover Our New Collection</h3>
+            <p className={`mt-5`} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quos ipsa, minus dignissimos reprehenderit aspernatur asperiores, voluptas exercitationem vero molestias sequi officiis culpa totam quae fuga nobis delectus sunt praesentium!</p>
+            <Button className='mt-10'>Buy Now</Button>
         </div>
     </main>
   )
