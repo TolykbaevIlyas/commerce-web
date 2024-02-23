272a1cca1d4835fa916ab410358f8d47d4e40326
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-02-23 15:26:58 +0600

    Create Footer

--- src/entities/CarouselCard/ui/CarouselCard.tsx
+++ src/entities/CarouselCard/ui/CarouselCard.tsx
@@ -7,6 +7,10 @@ const CarouselCard = ({title,description,image,className}:ICarouselCard) => {
   return (
     <div className={`${className} ${s.CarouselCard} `}>
         <Image src={image} alt={'photo'} height={700}/>
+        <div className={`${s.CarouselBlock}`}>
+          <h4>{title}</h4>
+          <p>{description}</p>
+        </div>
     </div>
   )
 }
--- src/widgets/Share/ui/Share.tsx
+++ src/widgets/Share/ui/Share.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import s from '../assets/styles/style.module.css'
 
 const Share = () => {
   return (
-    <div className=''>Share</div>
+    <div className='text-center mt-10'>
+      <h4 className={`${s.MainTitle} font-semibold`}>Share your setup with</h4>
+      <h2 className={`${s.MainHashtag} font-extrabold`}>#FuniroFurniture</h2>
+    </div>
   )
 }
 
