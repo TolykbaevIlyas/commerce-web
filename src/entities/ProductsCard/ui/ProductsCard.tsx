0989a8638482267de574ee6502ddfc040e9027cd
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-04-03 22:06:00 +0500

    refactor ProductCard

--- src/entities/ProductsCard/ui/ProductsCard.tsx
+++ src/entities/ProductsCard/ui/ProductsCard.tsx
@@ -6,14 +6,23 @@ import s from '../assets/css/style.module.css'
 import { Button } from '@/shared/ui/Button'
 
 const ProductsCard = ({name,price,img,shortDescription}:IProductCard) => {
+
+  const Btns = [
+    {id:1, name:"Share"},
+    {id:2, name:"Compare"},
+    {id:3, name:"Like"},
+  ]
+
   const [isMouseOver, setIsMouseOver] = useState(false)
 
   const handleMouseOver = () => {
     setIsMouseOver(true);
+
   }
 
   const handleMouseOut = () => {
     setIsMouseOver(false);
+
   }
 
   return (
@@ -24,17 +33,24 @@ const ProductsCard = ({name,price,img,shortDescription}:IProductCard) => {
             <p className={`font-extralight ${s.Description}`}>{shortDescription}</p>
             <p className={`font-bold ${s.price}`}>{price} $</p>
         </div>
-        {isMouseOver ? 
-        <div className=' flex justify-center items-center z-10 w-full h-full relative -top-full bg-black/40'> 
-          <Button variant='5' className='absolute hover:bg-primary hover:text-white duration-100'>
-            Add to Cart
-          </Button> 
-          <div className='flex absolute mt-24 gap-10'>
-            <Button variant='2' className='relative text-white font-bold hover:text-primary duration-100'>Share</Button>
-            <Button variant='2' className='relative text-white font-bold hover:text-primary duration-100'>Compare</Button>
-            <Button variant='2' className='relative text-white font-bold hover:text-primary duration-100'>Like</Button>
-          </div>
-        </div> : null}
+        {
+        isMouseOver ? 
+          <div className=' flex justify-center items-center z-10 w-full h-full relative -top-full bg-black/40'> 
+            <Button variant='5' className='absolute hover:bg-primary hover:text-white duration-100'>
+              Add to Cart
+            </Button> 
+            <div className='flex absolute mt-24 gap-10'>
+              {
+                Btns.map(
+                  (btn) => 
+                    <Button key={btn.id} variant='2' className='relative text-white font-bold hover:text-primary duration-100'>
+                      {btn.name}
+                    </Button>
+                )
+              }
+            </div>
+          </div> 
+          : null}
     </div>
   )
 }
